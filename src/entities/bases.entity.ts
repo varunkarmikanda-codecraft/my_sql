39001e1974d3e6dcc30c2cb78bda60ad7efc4ae9
varun-karmikanda <varunbkarmikanda@gmail.com>
2026-04-12 13:17:52 +0530

    added find and delete querries for db operations

--- src/entities/bases.entity.ts
+++ src/entities/bases.entity.ts
@@ -41,20 +41,51 @@ export abstract class BaseEntity implements IBaseEntity {
     // await db.execute(query, Object.values(this));
   }
 
-  static async findById<T extends BaseEntity, I extends IBaseEntity>(this: (new (entity: I) => T) & typeof BaseEntity, id: number): Promise<T | null> {
-    const query = `SELECT * FROM ${this.getTableName()} WHERE id = ?`;
+  static async findById<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T, id: number): Promise<T | null> {
+    const query = `SELECT * FROM ${Reflect.getMetadata(TABLE_METADATA_KEY, this)} WHERE id = ?`;
+    console.log(query);
+    return null;
     // const result = await db.execute(query, [id]);
     // const instance = new this(result[0]);
     // return instance;
+  }
+
+  static async findAll<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T): Promise<T[]> {
+    const query = `SELECT * FROM ${Reflect.getMetadata(TABLE_METADATA_KEY, this)}`;
     console.log(query);
+    return [];
+    // const result = await db.execute(query);
+    // const instance = new this(result);
+    // return instance;
+  }
+
+  static async findOne<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T , conditions: Partial<I>): Promise<T | null> {
+
+    const query = `SELECT * FROM ${Reflect.getMetadata(TABLE_METADATA_KEY, this)} WHERE ${Object.keys(conditions)} = ? LIMIT 1`;
+    console.log(query);
+    // const result = await db.execute(query, Object.values(conditions));
     return null;
   }
 
-  // TASKS:
-  // static async findAll<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T): Promise<T[]> 
-  // static async findOne<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T, conditions: Partial<I>): Promise<T | null> 
+  static async deleteById<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T, id: number): Promise<T[]> {
+    const query = `DELETE FROM ${Reflect.getMetadata(TABLE_METADATA_KEY, this)} WHERE id = ?`;
+    console.log(query);
+    // const result = await db.execute(query, [id]);
+    return []
+  }
 
-  // static async deleteById<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T): Promise<T[]> 
-  // static async deleteAll<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T): Promise<T[]> 
-  // static async deleteOne<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T, conditions: Partial<I>): Promise<T | null> 
+  static async deleteAll<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T): Promise<T[]> {
+    const query = `DELETE FROM ${Reflect.getMetadata(TABLE_METADATA_KEY, this)}`;
+    console.log(query);
+    // const result = await db.execute(query);
+    return [];
+  }
+
+  static async deleteOne<T extends BaseEntity, I extends IBaseEntity>(this: new (entity: I) => T, conditions: Partial<I>): Promise<T | null> {
+    const query = `DELETE FROM ${Reflect.getMetadata(TABLE_METADATA_KEY, this)} WHERE ${Object.keys(conditions)} = ? LIMIT 1`;
+    console.log(query);
+    // const result = await db.execute(query, Object.values(conditions))
+    return null;
+  }
+  
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import { Employee, type IEmployee } from "./entities/employee.entity.js";
 import { User, type IUser } from "./entities/user.entity.js";
 
-const mockUser: IUser = {
+const user: IUser = {
   id: 123,
   name: "Varun",
   address: "123 Docker Lane, Container City",
@@ -12,9 +13,34 @@ const mockUser: IUser = {
   updatedBy: 123
 };
 
-const newUser = new User(mockUser);
-
+const newUser = new User(user);
 newUser.save();
-
 User.findById(123)
+User.findAll()
+User.findOne({ id: 123 })
+User.deleteById(67);
+User.deleteAll();
+User.deleteOne({ name: 'varun' })
 
+console.log()
+
+const employee: IEmployee = {
+  id: 123,
+  name: "Varun",
+  position: "dev",
+  department: 'backend',
+  salary: 9999999,
+  createdAt: new Date("2026-01-01T10:00:00Z"),
+  createdBy: 123,
+  updatedAt: new Date("2026-04-10T12:00:00Z"),
+  updatedBy: 123
+};
+
+const newEmployee = new Employee(employee);
+newEmployee.save();
+Employee.findById(123)
+Employee.findAll()
+Employee.findOne({ name: 'john' })
+Employee.deleteById(26);
+Employee.deleteAll();
+Employee.deleteOne({ id: 6 })
